c56e8475f221a0db327fb37a92ea17d3ce4ba347
Capfund <onions193@gmail.com>
2026-02-08 22:04:11 +0700

    update retry strategy to ensure no missing assets

--- src/backend/core.py
+++ src/backend/core.py
@@ -39,10 +39,13 @@ class MinecraftCore:
         self.natives_dir = os.path.join(minecraft_dir, "natives")
         self.assets_dir = os.path.join(minecraft_dir, "assets")
         retry_strategy = Retry(
-            total=3,
-            backoff_factor=1,
+            total=5,
+            connect=5,
+            read=5,
+            backoff_factor=2,
             status_forcelist=[429, 500, 502, 503, 504],
-            allowed_methods=["GET"]
+            allowed_methods=["GET"],
+            raise_on_status=False
         )
         adapter = HTTPAdapter(max_retries=retry_strategy, pool_connections=10, pool_maxsize=10)
         self.session.mount("http://", adapter)
